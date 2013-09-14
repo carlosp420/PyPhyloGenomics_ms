069a6322b643dd8e103047af468a987db3d54c6b
Carlos Peña <mycalesis@gmail.com>
2013-09-14 14:13:21 +0300

    using pp

--- code/separate_by_index.py
+++ code/separate_by_index.py
@@ -3,6 +3,8 @@
 import glob;
 import os;
 from pyphylogenomics import NGS;
+import pp;
+import sys;
 
 
 # find reads matching our indexes and do separation
@@ -11,5 +13,24 @@ from pyphylogenomics import NGS;
 index_list = "data/indexes.fasta";
 folder = "output";
 levenshtein_distance = 0;
+
+# progressbar
+progressbar_width = 20;
+sys.stdout.write("Progress: [%s]" % (" " * progressbar_width))
+sys.stdout.flush();
+sys.stdout.write("\b" * (progressbar_width + 1))
+
+ppservers = ("*",)
+job_server = pp.Server(ppservers=ppservers, secret="123")
+jobs = []
+
 for gene_file in glob.glob(os.path.join("output", "gene*fastq")):
-    NGS.separate_by_index(gene_file, index_list, folder, levenshtein_distance);
+    jobs.append(job_server.submit(NGS.separate_by_index, (gene_file, index_list, folder, levenshtein_distance),(),()));
+
+for job in jobs:
+    sys.stdout.write("#")
+    sys.stdout.flush();
+    job()
+
+sys.stdout.write("\n")
+job_server.print_stats();
